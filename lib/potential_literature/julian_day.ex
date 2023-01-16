0a502e418b65cd3e162e1fc3708f6c2ffda915b5
Kevin Barrett <kb@kevinbarrett.org>
2023-01-16 08:15:41 -0500

    cleanup, fix empty trophy queries

--- lib/potential_literature/julian_day.ex
+++ lib/potential_literature/julian_day.ex
@@ -1,11 +1,9 @@
 defmodule PotentialLiterature.JulianDay do
-  def to_julian_day(date_or_datetime) do
-    date =
-      case date_or_datetime do
-        %Date{} = date -> date
-        %DateTime{} = dt -> dt |> DateTime.to_date()
-      end
+  def to_julian_day(%DateTime{} = dt) do
+    to_julian_day(dt |> DateTime.to_date())
+  end
 
+  def to_julian_day(%Date{} = date) do
     {year, _} = Date.year_of_era(date)
     year * 1000 + Date.day_of_year(date)
   end
